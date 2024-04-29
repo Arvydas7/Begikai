01ca3d068f5986fc719fff3312d74dcc8e9d20c6
lietinis <sarunasgerve11@gmail.com>
2024-04-29 22:04:51 +0300

    Veikianti muzika/efektai ir background fix
    - sutvarkytas fono isdestymas erdveje
    - solo/duo rezimuose muzika nustoja groti numirus
    - solo/duo rezimuose numirus skamba mirties garso efektas

--- Begikai/Assets/PlayerCollision.cs
+++ Begikai/Assets/PlayerCollision.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public AudioSource audioPlayerSFX;
+    public AudioSource audioPlayerBGM;
     private void OnCollisionEnter2D(Collision2D other){
         if (other.transform.tag == "Obstacle"){
             Destroy(gameObject);
             GameManager.Instance.GameOver();
+            audioPlayerSFX.Play();
+            audioPlayerBGM.Stop();
         }
     }
 }
